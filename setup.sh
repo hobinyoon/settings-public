d636eb3917d07d3c4cd004782975cde621b9913b
Hobin Yoon <hyoon@pinterest.com>
2024-09-05 06:06:24 +0000

    Update setup

---
+++ setup.sh
@@ -0,0 +1,4 @@
+ln -sf ~/code/hobinyoon/settings-public/.bash_profile ~/.bash_profile
+ln -sf ~/code/hobinyoon/settings-public/.bashrc.bash ~/.bashrc
+ln -sf ~/code/hobinyoon/settings-public/.vimrc ~/.vimrc
+ln -sf ~/code/hobinyoon/settings-public/.gitconfig-global ~/.gitconfig
