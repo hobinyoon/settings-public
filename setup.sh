7e4b98cd95a8808974fc4367802da932b5f22749
Hobin Yoon <hobinyoon@gmail.com>
2024-10-24 22:57:55 +0000

    update

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ ln -sf ~/code/hobinyoon/settings-public/.bash_profile ~/.bash_profile
 # Don't want to override what platform already provides
 #   ln -sf ~/code/hobinyoon/settings-public/.bashrc.bash ~/.bashrc
 # Instead, add the following in ~/.bashrc
-#   . ~/code/hobinyoon/settings-public/.bashrc
+#   . ~/code/hobinyoon/settings-public/.bashrc.bash
 
 ln -sf ~/code/hobinyoon/settings-public/.vimrc ~/.vimrc
 ln -sf ~/code/hobinyoon/settings-public/.gitconfig-global ~/.gitconfig
