4457dc8682618f3c7829c3d2b00feecc50a4ed1b
Hobin Yoon <hobinyoon@gmail.com>
2024-12-17 03:53:54 +0000

    Install fugitive

--- setup.sh
+++ setup.sh
@@ -7,3 +7,10 @@ ln -sf ~/code/hobinyoon/settings-public/.bash_profile ~/.bash_profile
 
 ln -sf ~/code/hobinyoon/settings-public/.vimrc ~/.vimrc
 ln -sf ~/code/hobinyoon/settings-public/.gitconfig-global ~/.gitconfig
+
+# Install vim-fugitive
+#   https://github.com/tpope/vim-fugitive
+mkdir -p ~/.vim/pack/tpope/start
+cd ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/fugitive.git
+vim -u NONE -c "helptags fugitive/doc" -c q
