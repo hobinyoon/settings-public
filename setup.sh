a02264dfc84c6db5e9f77a9d2f2c0aa58e7f03e4
Hobin Yoon <hobinyoon@gmail.com>
2025-02-24 04:20:26 +0000

    update

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
-ln -sf ~/code/hobinyoon/settings-public/.bash_profile ~/.bash_profile
+#!/bin/bash
 
-# Don't want to override what platform already provides
-#   ln -sf ~/code/hobinyoon/settings-public/.bashrc.bash ~/.bashrc
-# Instead, add the following in ~/.bashrc
-#   . ~/code/hobinyoon/settings-public/.bashrc.bash
+set -e
+
+ln -sf ~/code/hobinyoon/settings-public/.bash_profile ~/.bash_profile
 
 ln -sf ~/code/hobinyoon/settings-public/.vimrc ~/.vimrc
 ln -sf ~/code/hobinyoon/settings-public/.gitconfig-global ~/.gitconfig
+
+# Fix the file ownership because sometimes they are overwritten by git or htop
+sudo chown -R `whoami`:`id -gn` ~/.config
 
 # Install vim-fugitive
 #   https://github.com/tpope/vim-fugitive
@@ -14,3 +16,6 @@ mkdir -p ~/.vim/pack/tpope/start
 cd ~/.vim/pack/tpope/start
 git clone https://tpope.io/vim/fugitive.git
 vim -u NONE -c "helptags fugitive/doc" -c q
+
+echo ". ~/code/hobinyoon/settings-public/.bashrc.bash" >> ~/.bashrc
+
